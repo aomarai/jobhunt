afa09cac9ae09f0777589f542efaf3976ee84704
Ash Omaraie <ashomaraie@icloud.com>
2026-06-19 01:24:24 +0000

    Fixing ruff complaint about imports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,8 @@
-from app.models.models import User, Company, Application, Stage, Contact, Question, Reminder
-from app.models.junctions import StageQuestion
+from app.models.models import User as User
+from app.models.models import Company as Company
+from app.models.models import Application as Application
+from app.models.models import Stage as Stage
+from app.models.models import Contact as Contact
+from app.models.models import Question as Question
+from app.models.models import Reminder as Reminder
+from app.models.junctions import StageQuestion as StageQuestion
