3db2cc73fc52138f8646935dbf6306a184f1254b
Ash Omaraie <ashomaraie@icloud.com>
2026-06-19 02:32:49 +0000

    Fixing ruff action to give it permissions and adding noqa again with specific feature for env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 from app.database import Base
 from app.config import settings
 
-import app.models as models #noqa
+import app.models # noqa: F401
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
